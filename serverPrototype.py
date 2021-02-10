e0d7f2ae50fef6230b436413304a3438a5fd69b2
Benjamin Howard <bjhoward5555@gmail.com>
2021-02-10 13:18:58 +1300

    made script into a while loop

--- serverPrototype.py
+++ serverPrototype.py
@@ -2,6 +2,7 @@ import asyncio
 import requests
 import json
 import base64
+import time
 
 # Author: Benjamin Howard
 # date: 04-02-2021
@@ -17,13 +18,14 @@ userApplicationId = 2075 # boatsecure user application ID
 swarmParams={'deviceid': '1610', 'status': 0, 'direction': 'fromdevice', 'count': 1}
 # path for getting message information for a specific userId
 swarmMessagePath="/api/v1/messages/" + str(userApplicationId)
+swarmReceived = "/rxack/"
 
 # --- ThingsBoard Cloud API information -----------------------
 
 
 # base thingsboard cloudn url
 thingsBoardURL = "https://thingsboard.cloud/api/v1/"
-thingsBoardDeviceId = "HzPOXOov5Dy1LvSPyVGz"
+thingsBoardDeviceId = "TileId-1610"
 
 # things board telemetry path
 thingsBoardPath = thingsBoardDeviceId + "/telemetry"
@@ -33,7 +35,6 @@ thingsBoardPath = thingsBoardDeviceId + "/telemetry"
 # formats the returned swarm data, returns packetId and data
 def formatSwarmData(content):
     for message in content:
-        print(message)
         data = message["data"]
         packetId = message["packetId"]
 
@@ -41,9 +42,13 @@ def formatSwarmData(content):
         base64_bytes = data.encode('ascii')
         data_bytes = base64.b64decode(base64_bytes)
         data = data_bytes.decode('ascii')
-        dataObject = {'data': data}
 
-        print(dataObject, packetId)
+        # split the data object (temporary)
+        dataComponents = data.split(',')
+        # create the data structure to send
+        dataObject = {'latitude': dataComponents[1], 'longitude': dataComponents[2]}
+
+        print(data, dataObject, packetId)
         return (dataObject, packetId)
 
 
@@ -55,20 +60,25 @@ async def postToThingsBoard(url, path, dataObject):
 # api request to receive 1st unseen message in Swarm queue from a specific device
 async def getSwarmMessage(url, path, params):
     response = s.get(url + path, params=params)
-    print(response)
     print(response.json())
     return response.json()
 
+async def acknowledgeSwarmPacketReceived(packetId):
+    response = s.get(swarmURL + swarmMessagePath + swarmReceived + str(packetId))
+    print(response, response.content)
+
 
 async def main():
+    while True:
         # get a single message from swam API
         msgResponse = await getSwarmMessage(swarmURL, swarmMessagePath, swarmParams)
         # get the data and packetId from swarmAPI
         dataObject, packetId = formatSwarmData(msgResponse)
         # TODO: mark data as collected
-        print("packetId to confirm: " + str(packetId))
+        await acknowledgeSwarmPacketReceived(packetId)
         # send data to thingsBoard Cloud
         await postToThingsBoard(thingsBoardURL, thingsBoardPath, dataObject)
+        time.sleep(5)
 
 # --- LOGIN to SWARM API / START --------------------------------------
 
